342c43f6b15342e8d300be2b98732fbab60d987f
carmelariadas <carmen.mk.167@gmail.com>
2022-10-04 18:55:20 +0000

    terminado3

--- src/app.js
+++ src/app.js
@@ -8,4 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var ext = [".com", "pued.es", ".net"];
+
+  let domain;
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < ext.length; e++) {
+          domain = pronoun[p] + adj[a] + noun[n] + ext[e];
+          console.log(domain);
+        }
+      }
+    }
+  }
 };
